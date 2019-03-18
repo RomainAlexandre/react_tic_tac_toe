6540c9faddd46fe676acf787f0c81bb48a0e5dd5
Romain Alexandre <alexandre.romain06@gmail.com>
2019-03-18 08:44:36 +0000

    Make sure the production build is served under react_tic_tac_toe

---
+++ build/precache-manifest.dc16a2488988bc93ec87783a771f5b60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "22704b9c00a178857ff4",
+    "url": "/react_tic_tac_toe/static/js/runtime~main.ed74bf3f.js"
+  },
+  {
+    "revision": "74100e6c302ef6139b19",
+    "url": "/react_tic_tac_toe/static/js/main.5d34e0d0.chunk.js"
+  },
+  {
+    "revision": "036f1fd0e44e3723d5f1",
+    "url": "/react_tic_tac_toe/static/js/2.ecf5305d.chunk.js"
+  },
+  {
+    "revision": "74100e6c302ef6139b19",
+    "url": "/react_tic_tac_toe/static/css/main.6c349259.chunk.css"
+  },
+  {
+    "revision": "3a37b83fa8e5540ff75f341039b59973",
+    "url": "/react_tic_tac_toe/index.html"
+  }
+];
